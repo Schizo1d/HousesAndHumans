d174c2c254c9c224130aeca8d4f1e63ab89711ea
Schizo1d <Leopard731@mail.ru>
2025-01-30 23:06:35 +1000

    Обновление

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->nullable()->unique();
-            $table->string('avatar')->nullable();
+            $table->longText('avatar')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
