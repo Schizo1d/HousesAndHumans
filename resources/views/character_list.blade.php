9f9fd0931e9f222866e682d831e805503d2fcd99
Schizo1d <Leopard731@mail.ru>
2025-01-31 02:03:11 +1000

    Обновление

--- resources/views/character_list.blade.php
+++ resources/views/character_list.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Персонажи</title>
-    <link rel="stylesheet" href="/css/character_list.css">
+    <link rel="stylesheet" href="{{asset('css/character_list.css')}}">
     <link rel="stylesheet"
           href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css')}}">
 </head>
