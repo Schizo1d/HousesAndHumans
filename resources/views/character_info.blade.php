520cef3e47ca9937be6e494f1fead6a2844c8713
Schizo1d <Leopard731@mail.ru>
2025-01-31 23:06:30 +1000

    Обновление

--- resources/views/character_info.blade.php
+++ resources/views/character_info.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Персонажи</title>
     <link rel="stylesheet" href="{{ asset('css/character_list.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/character_info.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 
