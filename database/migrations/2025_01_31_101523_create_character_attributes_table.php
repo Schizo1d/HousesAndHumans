a33e62cc1fb85fc003da0383e00843e179b5b2a1
Schizo1d <Leopard731@mail.ru>
2025-01-31 20:43:10 +1000

    Обновление

--- database/migrations/2025_01_31_101523_create_character_attributes_table.php
+++ database/migrations/2025_01_31_101523_create_character_attributes_table.php
@@ -9,20 +9,11 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('character_attributes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('character_id');
-            $table->integer('strength')->default(10);
-            $table->integer('dexterity')->default(10);
-            $table->integer('constitution')->default(10);
-            $table->integer('intelligence')->default(10);
-            $table->integer('wisdom')->default(10);
-            $table->integer('charisma')->default(10);
             $table->timestamps();
-
-            $table->foreign('character_id')->references('id')->on('characters')->onDelete('cascade');
         });
     }
 
