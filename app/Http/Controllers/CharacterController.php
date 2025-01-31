0bb483ae93e805b58d5d3aec0ed6f72eda724b73
Schizo1d <Leopard731@mail.ru>
2025-02-01 01:08:59 +1000

    Обновление css файла

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -29,7 +29,7 @@ class CharacterController extends Controller
         }
 
         // Если пользователь не авторизован, редиректим на страницу входа
-        return redirect()->route('character_list')->with('success', 'Атрибуты успешно сохранены!');
+        return redirect()->route('character-list')->with('success', 'Атрибуты успешно сохранены!');
     }
 
     public function store(Request $request)
