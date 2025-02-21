6c08163f972245f74102a014248a169e501b9644
Schizo1d <Leopard731@mail.ru>
2025-02-22 01:08:40 +1000

    Обнова character_info

--- app/Models/CharacterAttribute.php
+++ app/Models/CharacterAttribute.php
@@ -14,6 +14,24 @@ class CharacterAttribute extends Model
         'intelligence',
         'wisdom',
         'charisma',
+        'athletics',         // Атлетика
+        'acrobatics',        // Акробатика
+        'sleight_of_hand',   // Ловкость рук
+        'stealth',           // Скрытность
+        'investigation',     // Анализ
+        'history',           // История
+        'arcana',            // Магия
+        'nature',            // Природа
+        'religion',          // Религия
+        'perception',        // Восприятие
+        'survival',          // Выживание
+        'medicine',          // Медицина
+        'insight',           // Проницательность
+        'animal_handling',   // Уход за животными
+        'performance',       // Выступление
+        'intimidation',      // Запугивание
+        'deception',         // Обман
+        'persuasion',        // Убеждение
     ];
 
     public function character()
