4b961d39e00b36dce8e8ed8241f535419777e48b
Schizo1d <Leopard731@mail.ru>
2025-03-17 15:16:21 +1000

    Обновление Web

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('main');
 });
-
 Route::get('/character_list', [CharacterController::class, 'index'])->name('character_list');
 
 Route::post('/character/{character}/attributes', [CharacterAttributeController::class, 'store'])
