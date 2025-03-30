8c02018e8a32fd9d50c3282241d253b317a38b4c
Schizo1d <Leopard731@mail.ru>
2025-03-31 01:11:29 +1000

    Добавление модального окно

--- public/js/modal-authorization.js
+++ public/js/modal-authorization.js
@@ -64,7 +64,9 @@ document.addEventListener("DOMContentLoaded", function () {
             let data = await response.json();
 
             if (data.success) {
+                setTimeout(() => {
                     window.location.href = data.redirect;
+                }, 1000); // Перенаправляем на главную
             } else {
                 console.error("Ошибка авторизации/регистрации:", data.message);
             }
