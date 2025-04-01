b4c722026175e919d14c828afcfaf81bae2708b8
Schizo1d <Leopard731@mail.ru>
2025-04-02 02:12:27 +1000

    обновляем персонажа

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/character/update-name', [CharacterController::class, 'updateName'])->name('character.updateName');
+Route::patch('/character/{character}', [CharacterController::class, 'update'])->name('character.update');
+
 Route::get('/', function () {
     return view('main');
 });
