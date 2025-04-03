036ff4cee4dd5a30b44cbe29444d77c2379e0e2a
Schizo1d <Leopard731@mail.ru>
2025-04-04 04:20:22 +1000

    обновляем персонажа

--- resources/views/character_info.blade.php
+++ resources/views/character_info.blade.php
@@ -88,7 +88,7 @@
                             <a href="#" class="skill-toggle" data-target="athletics">
                                 Атлетика:
                             </a>
-                            <button type="button" class="skill-button" id="athletics-button" onclick="document.querySelector('[data-target=athletics]').click()">
+                            <button type="button" class="dice-roll-button" id="athletics-button" onclick="document.querySelector('[data-target=athletics]').click()">
                                 <span id="athletics-value">{{ $character->attributes->athletics ?? 0 }}</span>
                             </button>
                         </div>
