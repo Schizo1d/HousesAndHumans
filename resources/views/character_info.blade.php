631582ed3a6fceea064e6813f15a633b8cd6569a
Schizo1d <Leopard731@mail.ru>
2025-05-01 20:44:42 +1000

    добавление уровней и калькулятора

--- resources/views/character_info.blade.php
+++ resources/views/character_info.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Персонажи</title>
     <link rel="stylesheet" href="{{ asset('css/character_info.css') }}">
+    <script src="{{ asset('js/character_info.js') }}" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
