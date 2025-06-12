ff21fce61edca685561b030e70ca7662692a7919
Schizo1d <Leopard731@mail.ru>
2025-06-12 18:57:12 +1000

    добавление правой части

--- resources/views/character_info.blade.php
+++ resources/views/character_info.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Персонажи</title>
     <link rel="stylesheet" href="{{ asset('css/character_info.css') }}">
-    <link rel="stylesheet" href="{{asset('css/character_content_right.css')}}}">
+    <link rel="stylesheet" href="{{asset('css/character_content_right.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 <body>
