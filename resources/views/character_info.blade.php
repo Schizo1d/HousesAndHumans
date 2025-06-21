2bd464862450391a325f7d40a308be426f12c400
Schizo1d <Leopard731@mail.ru>
2025-06-21 17:09:39 +1000

    Изменение  калькулятора

--- resources/views/character_info.blade.php
+++ resources/views/character_info.blade.php
@@ -2712,6 +2712,7 @@
                     showCustomAlert(message);
                     checkDeathSaveStatus();
                 });
+                console.log(document.getElementById('death-save-roll-btn'));
             });
 
             function updateDeathSavesCheckboxes() {
