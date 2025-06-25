5ee5243c617d2cbfd3443b223a9a9ae1e1bc3d86
Schizo1d <Leopard731@mail.ru>
2025-06-25 19:15:01 +1000

    Адаптация для телефона

--- resources/views/character_info.blade.php
+++ resources/views/character_info.blade.php
@@ -1400,8 +1400,9 @@
                 <button type="button" class="calc-button action-btn add-btn" onclick="calculateAndAdd()">ПРИБАВИТЬ</button>
                 <button type="button" class="calc-button action-btn subtract-btn" onclick="calculateAndSubtract()">ОТНЯТЬ</button>
                 <button type="button" class="calc-button action-btn level-up-btn" id="level-up-btn" onclick="levelUpCharacter()">ПОВЫСИТЬ</button>
-                <button type="button" class="calc-button action-btn level-down-btn" id="level-down-btn" onclick="levelDownCharacter()">ПОНИЗИТЬ</button>
                 <div class="calc-empty"></div>
+                <button type="button" class="calc-button action-btn level-down-btn" id="level-down-btn" onclick="levelDownCharacter()">ПОНИЗИТЬ</button>
+
             </div>
         </div>
     </div>
